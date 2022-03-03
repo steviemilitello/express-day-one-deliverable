fa4194fefb613563927900efae13fab11c21e9bf
STEVIEMILITELLO <steviemilitello@gmail.com>
2022-03-02 20:06:24 -0500

    magic 8 ball express application created

--- server.js
+++ server.js
@@ -31,6 +31,38 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
     res.send(`the tip is ${totalAmount}`)
 })
 
+// --- MAGIC 8 HALL -------------------------------------------------------------------------------------------
+
+const magicBallArray = [
+    "It is certain", 
+    "It is decidedly so", 
+    "Without a doubt", 
+    "Yes definitely",
+    "You may rely on it", 
+    "As I see it yes", 
+    "Most likely", 
+    "Outlook good",
+    "Yes", 
+    "Signs point to yes", 
+    "Reply hazy try again", 
+    "Ask again later",
+    "Better not tell you now", 
+    "Cannot predict now", 
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good", 
+    "Very doubtful"
+]
+
+app.get("/magic/:question", (req, res) => {
+    reqLog (req)
+    const response = Math.floor(Math.random() * magicBallArray.length)
+    const randomResponse = magicBallArray[response]
+    res.send(`<h1>${randomResponse}</h1>`)
+})
+
 // -- LISTEN FOR PORT -----------------------------------------------------------------------------------------
 
 app.listen(port, () => {
